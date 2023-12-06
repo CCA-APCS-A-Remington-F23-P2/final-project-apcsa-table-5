3ba6ce2e741ffb02844fc1f3736c2216438be1fb
rosenb5216 <152912908+rosenb5216@users.noreply.github.com>
2023-12-06 19:36:05 +0000

    added red raven, changed pipes

--- RavenRed.java
+++ RavenRed.java
@@ -1,9 +1,12 @@
+import java.io.File;
+import java.net.URL;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Image;
-import java.io.File;
 import javax.imageio.ImageIO;
 
-public class Pipes extends MovingThing{
-    System.out.println("FIX THIS CLASS");
+public class RavenRed extends Raven
+{
+  
 }
+
